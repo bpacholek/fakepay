99d6636b3a56518566525724dbd44d36e4ae7aa2
Bartosz Pacholek <bartosz@idct.pl>
2020-03-02 21:27:38 +0100

    Updated installation scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-composer install
-php bin/console doctrine:database:create
-php bin/console doctrine:schema:create
-php bin/console cache:clear
-yarn install
-yarn encore dev
-echo "Now run `symfony server:start`"
+composer install &&
+php bin/console doctrine:database:create &&
+php bin/console doctrine:schema:create &&
+php bin/console cache:clear &&
+yarn install &&
+yarn encore dev &&
+echo "Now run 'symfony server:start'"
